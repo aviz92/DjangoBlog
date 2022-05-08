24a67bcef3ff7c982afc5c812404d724115814a1
azaguri <avi210892@gmail.com>
2022-05-08 18:45:25 +0300

    ~

---
+++ reinstall_packages.bat
@@ -0,0 +1,6 @@
+echo Install PrivateInfrastructure:
+pip uninstall -y PrivateInfrastructure
+pip install -r requirements.txt
+
+echo Install LDAP:
+pip install C:\Users\HomePC\PycharmProjects\aviz92\ParkingOpeningSystem\MoreFiles\LDAP\python_ldap-3.4.0-cp39-cp39-win_amd64.whl
